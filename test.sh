e8d1cb9ecad09a6c1849a4ce317a2ceb68b5b292
Francesco Paci <francesco.paci@greenwaves-technologies.com>
2024-11-07 15:52:45 +0100

    Added argument to provide a binary, optional argument to provide the entry function for the execution and few checkings before run the commands

--- test.sh
+++ test.sh
@@ -1,6 +1,42 @@
+#!/bin/bash
+
 path=`pwd`
-echo $path
+echo "Set path to $path"
+
+BINARY=$1
+EXEC_ADDRESS=$2
+
+
+if [ -z "$BINARY" ]
+  then
+    echo "No argument supplied, you should at least supply the path of the binary to be flashed"
+    echo "Usage ./test.sh PATH_OF_BINARY [START_FUNCTION_ADDRESS]"
+    exit 1
+fi
+
+if [ -f "${BINARY}" ]
+then echo "${BINARY} binary to be flashed";
+else echo "${BINARY} is not a valid file";
+     exit 1
+fi
+
+
+
+FILESIZE=$(stat -c%s "$BINARY")
+echo "Size of $BINARY = $FILESIZE bytes."
 
 #./openocd_ubuntu2204/bin/openocd -d2 -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -c "load_and_start_binary /home/yao/projects/sdk/gap_sdk/examples/gap9/basic/helloworld/build/helloworld 0x1c010160"
 
-./openocd_ubuntu2204/bin/openocd -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -f "$path/openocd_tools/tcl/flash_image.tcl" -c "gap9_flash_raw $path/test_elf/blink_led_mram.bin 60064 $path/openocd_tools/gap_bins/gap_flasher-gap9_evk-mram.elf 0x2000; exit;"
+./openocd_ubuntu2204/bin/openocd -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -f "$path/openocd_tools/tcl/flash_image.tcl" -c "gap9_flash_raw ${BINARY:P} $FILESIZE $path/openocd_tools/gap_bins/gap_flasher-gap9_evk-mram.elf 0x2000; exit;"
+
+
+if [ -z "$EXEC_ADDRESS" ]
+  then
+    echo "If you want to execute the binary you have to provide the address of the _start function in hexadecimal format (e.g. 0x1c010150)"
+    echo "Usage ./test.sh PATH_OF_BINARY [START_FUNCTION_ADDRESS]"
+    echo "You can use this command to get the _start function entry address: riscv32-unknown-elf-objdump multi_spi --source | grep \<_start\>"
+    exit 0
+fi
+
+
+./openocd_ubuntu2204/bin/openocd -d0 -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -c "load_and_start_binary  ${BINARY:P} $EXEC_ADDRESS"
