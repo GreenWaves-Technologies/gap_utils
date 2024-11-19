4af305c33c1789a36ba8f298537d4b17a2ea3461
Francesco Paci <francesco.paci@greenwaves-technologies.com>
2024-11-19 16:44:01 +0100

    Updated with MRAM and OCTOSPI arguments to flash multiple images and an argument to execute ELF from JTAG

--- README.md
+++ README.md
@@ -1,32 +1,76 @@
 # Gap Utils
 
-This repository provides essential utilities for flashing and executing a GAP9 binary on a board.
+This repository provides essential utilities for flashing and executing a GAP9 application on a board. 
+
+The GAP9 build system generates three files:
+- An ELF with the code of the application that can be executed from MRAM or form JTAG 
+- An MRAM image which, depending on the memory layout, contains code and optionally data partitions
+- An optional OCTOSPI Flash Image which usually contains only data, but could also contain code, since GAP9 could also boot from OCTOSPI Flash
+
+
+These three files are generated in the build folder of the application:
+
+- The Application ELF has the application name which is defined in the CMakeLists.txt under the variable TARGET_NAME.
+- mram.bin_0 
+- flash.bin_0 
+
+For example for the blink led example the build folder will contain:
+
 
 ## Getting Started
 
-Few test binaries are included to verify that flashing and running works correctly on a GAP9 EVK. To flash and execute a "blink LED" example, you can use the following command:
+Few test binaries are included to verify that flashing and running works correctly on a GAP9 EVK. 
+
+
+### Blink LED 
+To flash the "blink LED" example, you can use the following command:
 
 ```bash
-./flash_and_execute.sh test_elf/blink_led.elf 0x1c010150
+./flash_and_execute.sh --mram_img test_elf/blink_led_mream.bin 
+```
+To test is you need to shortcut the jumper BOOT1 (check naming behind the EVK) and reset the board with the reset button. If the LED blinks the flash process has run successfully.
+
+### Hello World
+
+This tool also permits to execute code from JTAG without the need of flash anything into the board, to do so you can use the following options:
+
+```bash
+./flash_and_execute.sh --exec test_elf/helloworld --addr 0x1c010150
+```
+You have to provide the ELF and the address of the _start function. To find this address there is command provided in the following section. 
+
+### Mobilenet
+
+In case of multiple flash usage and need to debug printf on jtag this example shows how to flash the two images to MRAM and OCTOSPI Flash and execute the elf code from jtag:
+
+```bash
+./flash_and_execute.sh --mram_img test_elf/mobilenet_mram.bin_0 --flash_img test_elf/mobilenet_flash.bin_0  --exec test_elf/mobilenet --addr 0x1c0101e0
 ```
 
+In this example NN paramters are copied into MRAM and OCTOSPI flash along with all other images partitions. Which are then used from the ELF to execute the application. 
+Along with the elf provided with the argument --exec you need to also provide the address of the _start function. To find this address there is command provided in the following section. The execution will load an image from JTAG and execute Mobilenet showing the detected class and the per layer performances. 
+
+This application cannot run from mram since the printf included will avoid the application to run. They must be redirected to UART or None to be used. 
 
 ## Usage 
 
+```
+Usage: ./flash_and_execute [ -m | --mram_img mram_img_file ]
+                           [ -f | --flash_img flash_img_file ]
+                           [ -e | --exec elf_file -a | --addr 0x1c0XXXXX ]
+                           [ -h | --help  ]
+```
 
-Usage: `./flash_and_execute.sh BINARY_FILE [_START_ADDRESS_HEX]`
+- `-m|--mram_img mram_img_file`: is the relative or absolute path to the EMRAM image to be flashed onto GAP9.
 
-- `BINARY_FILE`: is the relative or absolute path to the binary file to be flashed onto GAP9.
+- ` -f|--flash_img flash_img_file`: is the relative or absolute path to the OCTOSPI image to be flashed.
 
-- `_START_ADDRESS_HEX` (optional): The hexadecimal address of the _start function (the entry point of the executable). If provided, this address will be used to execute the binary through JTAG.
-To find the correct start address, you can use the RISC-V GCC toolchain with the following command:
-
-- is an optional argument containing the hexadecimal address of the function _start (the entry point of the executable).If it is present it will use the entry address to execute the binary through jtag. To found the correct address you can use riscv32 gcc tolchain with the following command: `riscv32-unknown-elf-objdump multi_spi --source | grep \<_start\>"`
+- ` -e|--exec elf_file -a|--addr 0x1c0XXXXX ` elf_file is the path of the elf to executed through JTAG and 0x1c0XXXXX is the hexadecimal address of the function _start (the entry point of the executable). Both arguments must be provided to execute the ELF through JTAG. To found the correct address you can use riscv32 gcc tolchain with the following command: `riscv32-unknown-elf-objdump multi_spi --source | grep \<_start\>"`
 
 The riscv32 gcc toolchain can be found [here](https://github.com/GreenWaves-Technologies/gap_gnu_toolchain)
 
 
-## Limitations
+## Known Limitations
 
-For the moment only Ubuntu 22.04 is supported. Next releases will also support windows 11. 
-
+- Only Ubuntu 22.04 is supported. Next releases will also support windows 11. 
+- This tool can only flash one image per flash type, next releases will be able to handle multiple images per flash with different flash addresses.
--- flash_and_execute.sh
+++ flash_and_execute.sh
@@ -1,41 +1,129 @@
 #!/bin/bash
 
-path=`pwd`
-echo "Set path to $path"
+# More safety, by turning some bugs into errors.
+set -o errexit -o pipefail -o noclobber -o nounset
 
-BINARY=$1
-EXEC_ADDRESS=$2
-
-
-if [ -z "$BINARY" ]
-  then
-    echo "No argument supplied, you should at least supply the path of the binary to be flashed"
-    echo "Usage ./test.sh PATH_OF_BINARY [START_FUNCTION_ADDRESS]"
+# ignore errexit with `&& true`
+getopt --test > /dev/null && true
+if [[ $? -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
     exit 1
 fi
 
-if [ -f "${BINARY}" ]
-then echo "${BINARY} binary to be flashed";
-else echo "${BINARY} is not a valid file";
-     exit 1
+path=`pwd`
+echo "Set path to $path"
+
+
+help()
+{
+    echo "Usage: ./flash_and_execute [ -m | --mram_img mram_img_file ]
+                           [ -f | --flash_img flash_img_file ]
+                           [ -e | --exec elf_file -a | --addr 0x1c0XXXXX ]
+                           [ -h | --help  ]"
+    exit 2
+}
+
+function getopts-extra () {
+    declare i=1
+    # if the next argument is not an option, then append it to array OPTARG
+    while [[ ${OPTIND} -le $# && ${!OPTIND:0:1} != '-' ]]; do
+        OPTARG[i]=${!OPTIND}
+        let i++ OPTIND++
+    done
+}
+
+
+# option --output/-o requires 1 argument
+LONGOPTS=mram_img:,flash_img:,exec:,addr:,help
+OPTIONS=m:,f:,e:,a:,h
+
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+# -if getopt fails, it complains itself to stdout
+PARSED=$(getopt -a -n test_new_flasher --options $OPTIONS --longoptions $LONGOPTS -- "$@")
+
+VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
+
+if [ "$VALID_ARGUMENTS" -eq 0 ]; then
+  help
 fi
 
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
 
 
-FILESIZE=$(stat -c%s "$BINARY")
-echo "Size of $BINARY = $FILESIZE bytes."
+m=n f=n e=n addr=n
+# now enjoy the options in order and nicely split until we see --
+while true; do
+    case "$1" in
+        -m|--mram_img)
+            m=$2
+            shift 2
+            ;;
+        -f|--flash_img)
+            f=$2
+            shift 2
+            ;;
+        -e|--exec)
+            e=$2
+            shift 2
+            ;;
+        -a|--addr)
+            addr=$2
+            shift 2
+            ;;
+        -h | --help)
+            help
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Unexpected option: $1"
+            help
+            ;;
+    esac
+done
 
-./openocd_ubuntu2204/bin/openocd -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -f "$path/openocd_tools/tcl/flash_image.tcl" -c "gap9_flash_raw ${BINARY:P} $FILESIZE $path/openocd_tools/gap_bins/gap_flasher-gap9_evk-mram.elf 0x2000; exit;"
+#handle non-option arguments
+# if [[ $# -ne 1 ]]; then
+#     echo "$0: A single input file is required."
+#     exit 4
+# fi
+
+#echo "exec:  $e $addr, flash_img: $f, mram_img: $m"
 
 
-if [ -z "$EXEC_ADDRESS" ]
-  then
-    echo "If you want to execute the binary you have to provide the address of the _start function in hexadecimal format (e.g. 0x1c010150)"
-    echo "Usage ./test.sh PATH_OF_BINARY [START_FUNCTION_ADDRESS]"
-    echo "You can use this command to get the _start function entry address: riscv32-unknown-elf-objdump multi_spi --source | grep \<_start\>"
-    exit 0
+## Flash INTO MRAM
+if [[ "$m" != "n" ]] && [ -f $m ]
+then
+  FILESIZE=$(stat -c%s "$m")
+  # The content of $m is different from "n" and is a file, then get the size and flash it
+  printf "\n\nFlashing into MRAM $m of size $FILESIZE at defulat Address 0x2000\n\n"
+
+  ./openocd_ubuntu2204/bin/openocd -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -f "$path/openocd_tools/tcl/flash_image.tcl" -c "gap9_flash_raw ${m} $FILESIZE $path/openocd_tools/gap_bins/gap_flasher-gap9_evk-mram.elf 0x2000; exit;"
+
 fi
 
-## Should the first argument be -d0 or -d2? 
+## Flash INTO OCTOSPI FLash
+if [[ "$f" != "n" ]] && [ -f $f ]
+then
+  FILESIZE=$(stat -c%s "$f")
+  # The content of $f is different from "n" and is a file, then get the size and flash it
+  printf "\n\nFlashing into OCTOSPI Flash $f of size $FILESIZE at defulat Address 0x2000\n\n"
 
-./openocd_ubuntu2204/bin/openocd -d0 -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -c "load_and_start_binary  ${BINARY:P} $EXEC_ADDRESS"
+  ./openocd_ubuntu2204/bin/openocd -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -f "$path/openocd_tools/tcl/flash_image.tcl" -c "gap9_flash_raw ${f} $FILESIZE $path/openocd_tools/gap_bins/gap_flasher-gap9_evk.elf 0x2000; exit;"
+
+fi
+
+## Execute app from JTAG
+if [[ "$e" != "n" ]] && [ -f $e ] && [[ "$addr" != "n" ]]
+then
+  # The content of $f is different from "n" and is a file, then get the size and flash it
+  printf "\n\nExecuting ELF $e with START address at $addr\n\n"
+
+  ./openocd_ubuntu2204/bin/openocd -d0 -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -f "$path/openocd_tools/tcl/gapuino_ftdi.cfg" -f "$path/openocd_tools/tcl/gap9revb.tcl" -c "load_and_start_binary  $e $addr"
+
+fi
